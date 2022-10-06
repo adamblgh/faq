ca10faa9dbc52385b932c3304b0afc626c516662
Your Name <you@example.com>
2022-10-06 14:11:16 +0200

    first

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="container border mt-5 w-75">
       <div className="row">
-        <div className="col-md-6 p-3 border"><h1>Questions And Answers About Login</h1></div>
+        <div className="col-md-6 p-3 border"><h1>Questions And Answers About Login </h1></div>
         <div className="col-md-6 border">
           <Questions data={data}/>
         </div>
